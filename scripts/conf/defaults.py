23b4fac155cd6e888246e7fa89d30726fd697b55
Alex Custura <a.custura@student.vu.nl>
2018-04-07 17:03:01 +0200

    Updated config settings

--- scripts/conf/defaults.py
+++ scripts/conf/defaults.py
@@ -5,7 +5,7 @@ master_hostname = "alex"
 # IP address of master VM (string)
 master_ip = "10.140.0.166"
 # number of slaves to create (integer)
-num_slaves = 1
+num_slaves = 2
 # unique name for the cluster (string)
 cluster_name = "aca540"
 # templated id to use for the slaves (string)
@@ -13,9 +13,9 @@ slave_template = "476"
 # temp file to hold slave hostnames (string)
 filename = "/tmp/slaves"
 # remote username on master e.g. admin or root
-remote_username = "alex"
+remote_username = "aca540"
 # hadoop installation directory
-hadoop_dir = "/usr/local/hadoop-2.7.5/"
+hadoop_dir = "/usr/local/hadoop/"
 # Spark installation directory
 spark_dir = "/usr/local/spark/"
 # Hibench installation directory
