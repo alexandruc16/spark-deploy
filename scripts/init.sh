e68d0eeef740a8c2c3f8259bca5f78986290bd03
Alex Custura <a.custura@student.vu.nl>
2018-04-07 13:54:02 +0200

    Fixed typo

--- scripts/init.sh
+++ scripts/init.sh
@@ -97,7 +97,7 @@ echo -e "${YELLOW}Installing Development Tools${NC}"
 sudo apt-get -y install gcc make flex bison byacc git maven
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823 &>> /var/log/context.log
-sudp apt-get update &>> /var/log/context.log
+sudo apt-get update &>> /var/log/context.log
 sudo apt-get -y install sbt &>> /var/log/context.log
 echo -e "${GREEN}*********** dev tools Done ************${NC}"
 
