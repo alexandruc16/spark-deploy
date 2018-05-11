d9eba3398f7424e1628e5a04913c393861548f78
Alex Custura <a.custura@student.vu.nl>
2018-05-11 16:45:26 +0200

    More bugfixing

--- scripts/spark_deploy.py
+++ scripts/spark_deploy.py
@@ -187,7 +187,7 @@ def configure_hadoop(hadoop_dir, master_hostname, master_ip, slaves_dict, remote
     ssh_commands += 'sudo echo \'%s\' >> masters\n' % master_hostname
     
     for slave_hostname in slaves_dict.keys():
-        ssh_commands += 'sudo echo \'%s\' >> slaves\n'
+        ssh_commands += 'sudo echo \'%s\' >> slaves\n' % slave_hostname
     
     issue_ssh_commands(slaves_dict.values(), ssh_commands, remote_username, master_ip)
     print('Hadoop configured!')
