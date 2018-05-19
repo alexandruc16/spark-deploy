8560a58fc8cdce63760b13606f070d9471c9d189
Alex Custura <a.custura@student.vu.nl>
2018-05-19 13:37:18 +0200

    Moved bandwidth throttling scripts to separate folder

--- scripts/bandwidth_throttler/throttle_bandwidth_distribution.py
+++ scripts/bandwidth_throttler/throttle_bandwidth_distribution.py
@@ -1,6 +1,9 @@
 import argparse
 import itertools
+import logging
+import logging.handlers
 import numpy as np
+import os
 import sys
 from subprocess import Popen, PIPE
 from time import sleep
@@ -15,25 +18,39 @@ def generate_random_bandwidth_value(distribution):
     return np.random.uniform(low, high + 1.0)
 
 
-def generate(port, hosts distribution, filename):
+def generate(port, hosts, distribution, filename):
+    log = 'New bandwidth values:\n'
     f = open(filename, "w")
     
     for pair in itertools.permutations(hosts, 2):
         bandwidth_value = generate_random_bandwidth_value(distribution)
-        print >> f, "%s:%s:%f" % (pair[0], pair[1], bandwidth_value)
+        val = "%s:%s:%f" % (pair[0], pair[1], bandwidth_value)
+        print >> f, val
+        log += "%s\n" % val
     
     f.close()
     
-    result = Popen(["python", "shape_traffic_client.py", "--port"
-        port, "--set-file", filename],
-        stdout=PIPE).communicate()[0]
+    #result = Popen(["python", "shape_traffic_client.py", "--port",
+    #    port, "--set-file", filename],
+    #    stdout=PIPE).communicate()[0]
+    
+    logging.info(log)
+    
+    
+def init_logging():
+    log_filename = 'bandwidth_history.log'
+    logging.basicConfig(filename=log_filename, level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
+    
+    if os.path.isfile(log_filename):
+        handler = logging.handlers.RotatingFileHandler(log_filename, mode='w', backupCount=5)
+        handler.doRollover()
 
 
 def main():
     parser = argparse.ArgumentParser(description="Throttle cluster bandwidth "
                                      "according to a distribution.", 
                                      epilog = "Example Usage: "
-                                     "./throttle-bandwidth-distribution -p 2345 --hosts 10.10.10.10 10.10.10.11 --distribution 60 150 260 380 650 -i 1000")
+                                     "python throttle_bandwidth_distribution.py -p 2345 --hosts 10.10.10.10 10.10.10.11 --distribution 60 150 260 380 650 -i 1000")
     parser.add_argument("-p", "--port", metavar="", dest="port",
                         type=int, action="store", required=True,
                         help="Port number for bandwidth-throttler communication")                                 
@@ -55,13 +72,16 @@ def main():
         parser.print_help()
         sys.exit(0)
         
+    init_logging()
+        
     if args.interval is None:
         generate(args.port, args.hosts, args.distribution, args.output_filename)
     else:
+        sleep_seconds = args.interval / 1000
         #TODO  maybe something more precise
         while True:
             generate(args.port, args.hosts, args.distribution, args.output_filename)
-            sleep(args.interval)
+            sleep(sleep_seconds)
 
 
 if __name__ == "__main__":
