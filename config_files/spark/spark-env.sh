7d19c92372de34c6aa165befd902eed1f452547f
Alex Custura <a.custura@student.vu.nl>
2018-05-31 18:32:33 +0200

    Cleanup spark work folders

--- config_files/spark/spark-env.sh
+++ config_files/spark/spark-env.sh
@@ -1,3 +1,4 @@
 SPARK_WORKER_MEMORY=10g
 SPARK_WORKER_CORES=4
 SPARK_WORKER_INSTANCES=2
+SPARK_WORKER_OPTS="-Dspark.worker.cleanup.enabled=true -Dspark.worker.cleanup.interval=900 -Dspark.worker.cleanup.appDataTtl=1800"
