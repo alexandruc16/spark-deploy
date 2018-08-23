d884bd344a0194caf4cfd5a0b348951f7cfe57de
Alex Custura <a.custura@student.vu.nl>
2018-08-23 21:27:11 +0200

    TPC-DS script updates

--- scripts/utils/tpcds/prepare.scala
+++ scripts/utils/tpcds/prepare.scala
@@ -1,16 +1,17 @@
+import com.databricks.spark.SparkContext
 val sc: SparkContext // should have been created after running spark-shell
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 // this is used to implicitly convert an RDD to a DataFrame.
 import sqlContext.implicits._
-import com.databricks.spark.sql.perf.tpcds.Tables
+import com.databricks.spark.sql.perf.tpcds.TPCDSTables
 // Tables in TPC-DS benchmark used by experiments.
 // dsdgenDir is the directory for dsdgen (you need to compile dsdgen first).
 // scalefactor is the amount of data, GB
 val tables = new Tables(sqlContext, "/opt/tpcds-kit/tools", 32)
 // Generate data.
-tables.genData("hdfs:///tpcds", "parquet", true, false, false, false, false)
+tables.genData("hdfs:///tpcds", "parquet", true, false, false, false)
 // Create metastore tables in a specified database for your data.
 // After the table is created, automatically switch to the created database "sparktest"
-tables.createExternalTables("hdfs:///tpcds", "parquet", "finaltest", false)
+tables.createExternalTables("hdfs:///tpcds", "parquet", "finaltest", false, false)
 // Create a temporary table
-tables.createTemporaryTables(location, format)
+tables.createTemporaryTables("hdfs:///tpcds", "parquet")
--- scripts/utils/tpcds/run.scala
+++ scripts/utils/tpcds/run.scala
@@ -1,5 +1,8 @@
+import com.databricks.spark.SparkContext
+val sc: SparkContext // should have been created after running spark-shell
+val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 // Set TPC-DS experiment
 import com.databricks.spark.sql.perf.tpcds.TPCDS
 val tpcds = new TPCDS (sqlContext = sqlContext)
 // Run the test, the test set is specified as tpcds1_4
-val experiment = tpcds.runExperiment(tpcds.tpcds1_4Queries)
+val experiment = tpcds.runExperiment(tpcds.tpcds1_4Queries, iterations=10)
